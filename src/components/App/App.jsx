29947d55fcb7f99c5fd99d82f73db31cbf8a164d
PiganS <pigggggan@gmail.com>
2023-10-10 02:07:54 +0300

    profileAndStatistics

---
+++ src/components/App/App.jsx
@@ -0,0 +1,21 @@
+import user from "data/user.json";
+import data from "data/data.json";
+import friends from "data/friends.json";
+import transactions from "data/transactions.json";
+import { Profile } from "components/Profile/Profile";
+import { Statistics } from "components/Statistics/Statistics";
+
+
+export const App = () => {
+  return (
+  <div>
+  <Profile 
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+  />
+  <Statistics title="Upload stats" stats={data} />
+  </div>
+  )};
